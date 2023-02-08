1fa10f4fdddd41ca1ab7d1cf890e73a0844dd785
Min Thu Khant <minthukhant.it@gmail.com>
2023-02-08 22:55:46 +0630

    switch component added

--- src/components/switch/index.tsx
+++ src/components/switch/index.tsx
@@ -3,19 +3,17 @@ import Switch from "react-switch"
 import styles from './styles.module.css'
 
 type Props = {
-    className? : string,
+    className?: string,
     label?: string,
     onChange: any,
     checked: boolean
 }
 
-const SwitchComponent: React.FC<Props> = ({className, label, ...Props}) => {
+const SwitchComponent: React.FC<Props> = ({ className, label, ...Props }) => {
     return (
         <div className={`${styles.container} ${className}`}>
-            <label>
-                <span>{label}</span>
-                <Switch uncheckedIcon={false} checkedIcon={false} height={20} width={45} {...Props} />
-            </label>
+            <span className={styles.label}>{label}</span>
+            <Switch uncheckedIcon={false} checkedIcon={false} height={20} width={45} {...Props} />
         </div>
     )
 }
