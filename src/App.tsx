83ab6fd116150dc5708ef28a08f5390ed2adfb36
Min Thu Khant <minthukhant.it@gmail.com>
2023-02-08 19:39:40 +0630

    app file theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Router from './routes/routes'
 
 function App() {
   return (
-    <div className='w-screen h-screen overflow-hidden' >
+    <div className='w-screen h-screen overflow-hidden bg-black2 text-text' >
       <Router/>
     </div>
   )
