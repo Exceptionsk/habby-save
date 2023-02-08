38c69583ac9c63495819800eda731eb38da01366
Min Thu Khant <minthukhant.it@gmail.com>
2023-02-08 19:37:17 +0630

    update header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,15 +7,18 @@ type Props = {
     title?: string,
     className?: string,
     backButton?: boolean,
-    children?: React.ReactNode,
+    rightComponents?: JSX.Element,
 }
 
-const Header: React.FC<Props> = ({ backButton, title }) => {
+const Header: React.FC<Props> = ({ backButton, title, rightComponents }) => {
     const navigate = useNavigate()
     return (
         <div className={`${styles.container}`}>
             {backButton && <div className={styles.back} onClick={() => { navigate(-1) }}><IoIosArrowBack size={24} /></div>}
             <span className={`${styles.title}`}>{title}</span>
+            <div className={styles.right_components}>
+                {rightComponents}
+            </div>
         </div>
     )
 }
